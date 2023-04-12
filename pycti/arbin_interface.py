614d6cc2e5647fc7c70685f55d8666cf9586d9da
Chintan Pathak <chintan.pathak@gmail.com>
2023-04-12 10:59:13 -0700

    Login is looking much better

--- pycti/arbin_interface.py
+++ pycti/arbin_interface.py
@@ -25,7 +25,7 @@ class ArbinInterface:
         """
 
         # Channels are zero indexed within CTI so we must subtract one here.
-        self.channel = config['channel'] -1
+        self.channel = config['channel'] - 1
         self.config = config
         self.__sock = None
 
@@ -42,11 +42,11 @@ class ArbinInterface:
 
         if self.__verify_config():
             if self.__create_connection():
-                pass
-                #if self.__arbin_login():
-                #    success = True
-        return self.__arbin_login()
-        
+                if self.__arbin_login():
+                    success = True
+
+        return success
+
     def read_status(self) -> dict:
         """
         Method to read the status of the channel defined in the config.
@@ -57,7 +57,7 @@ class ArbinInterface:
             A dictionary detailing the status of the channel. Returns None if there is an issue.
         """
         return {}
-        
+
     def __verify_config(self) -> bool:
         """
         Verifies that the config passed on construction is valid.
@@ -81,7 +81,6 @@ class ArbinInterface:
                 return False
         logger.info("Verified config")
         return True
-        
 
     def __arbin_login(self) -> bool:
         """
@@ -97,21 +96,45 @@ class ArbinInterface:
 
         username = '123'
         password = '123'
-        login_msg = bytearray([])
-        login_msg += struct.pack('<Q', Constants.MSG.HEADER)
-        login_msg += struct.pack('<L', 4+4+32+32+2)
-        login_msg += struct.pack('<LL', Constants.MSG.COMMAND_CODES["USER_LOGIN_CMD"], 0x00000000)
-        login_msg += struct.pack('<{}s'.format(len(username)), username.encode('ascii'))
-        login_msg += bytearray([0x00 for i in range(32-len(username))])
-        login_msg += struct.pack('<{}s'.format(len(password)), password.encode('ascii'))
-        login_msg += bytearray([0x00 for i in range(32-len(password))])
-        login_msg += struct.pack('<H', sum(login_msg))
 
-        response = self.__send_receive_msg(login_msg)
-        success = True
+        # # Prepare the pieces of the login message
+        packed_header = struct.pack(
+            Constants.TX_MSG.HEADER_FORMAT, Constants.TX_MSG.HEADER)
+        packed_msg_length = struct.pack(
+            Constants.TX_MSG.MSG_LENGTH_FORMAT, Constants.TX_MSG.MSG_LENGTH['LOGIN'])
+        packed_command_code = struct.pack(
+            Constants.TX_MSG.COMMAND_CODE_FORMAT, Constants.TX_MSG.COMMAND_CODES["LOGIN"])
+        packed_command_code_extended = struct.pack(
+            Constants.TX_MSG.EXTENDED_COMMAND_FORMAT, Constants.TX_MSG.EXTENDED_COMMAND_CODE)
+        packed_username = struct.pack(
+            Constants.TX_MSG.STRING_FORMAT, username.encode(Constants.TX_MSG.STRING_ENCODING))
+        packed_password = struct.pack(
+            Constants.TX_MSG.STRING_FORMAT, password.encode(Constants.TX_MSG.STRING_ENCODING))
+        # Put the message together
+        login_msg_tx = packed_header + packed_msg_length + packed_command_code + \
+            packed_command_code_extended + packed_username + packed_password
+        # Add the checksum at the end
+        login_msg_tx += struct.pack('<H', sum(login_msg_tx))
 
-        return response
+        # Receive message response and parse it
+        login_msg_rx = self.__send_receive_msg(login_msg_tx)
+        if login_msg_rx:
+            # Get login result
+            login_result = struct.unpack(
+                Constants.RX_MSG.LOGIN.RESULT_FORMAT,
+                login_msg_rx[Constants.RX_MSG.LOGIN.RESULT_START_BYTE:Constants.RX_MSG.LOGIN.RESULT_END_BYTE])[0]
+            if login_result == Constants.RX_MSG.LOGIN.SUCESS_RESULT_CODE:
+                success = True
+                logger.info("Login success!")
+            elif login_result == Constants.RX_MSG.LOGIN.ALREADY_LOGGED_IN_CODE:
+                success = True
+                logger.info("Already logged in!")
+            elif login_result == Constants.RX_MSG.LOGIN.FAIL_RESULT_CODE:
+                logger.error("Login failed for provided credentials!")
+            else:
+                logger.error("Unknown login result response code")
 
+        return success
 
     def __create_connection(self) -> bool:
         """
@@ -128,7 +151,7 @@ class ArbinInterface:
             self.__sock = socket.socket(
                 socket.AF_INET, socket.SOCK_STREAM
             )
-            self.__sock.settimeout(Constants.MSG.TIMEOUT_S)
+            self.__sock.settimeout(Constants.TX_MSG.TIMEOUT_S)
             self.__sock.connect(
                 (self.config['arbin_ip'], self.config['arbin_port'])
             )
@@ -142,55 +165,37 @@ class ArbinInterface:
 
     def __send_receive_msg(self, tx_msg):
         """
-        Sends the passed message to the MITS server and receives the response.
+        Sends the passed message and receives the response.
 
         Parameters
         ----------
-        tx_msg_bytearray: bytearray
-            Msg to be sent to the server.
-        
+        tx_msg : bytearray
+            Message to send to the server.
+
         Returns
         -------
-        bytearray:
-            Response from the server.
+        rx_msg : bytearray
+            Response message from the server.
         """
-        '''
-        
-        self.__sock.send(tx_msg_bytearray)
-        data = b''
-        incoming = self.__sock.recv(Constants.MSG.BUFFER_SIZE_BYTES)
-        data_len = struct.unpack('<L', incoming[8:12])[0]
-        data += incoming
-        while len(data) < data_len:
-            try:
-                print("here")
-                incoming = self.__sock.recv(Constants.MSG.BUFFER_SIZE_BYTES)
-                print("Now here")
-                data += incoming
-            except:
-                logger.error("Error receiving msg")
-                break
-        # return self.sock.recv(MSG_BUFFER_SIZE)
-        return data
-        '''
 
         rx_msg = b''
         send_msg_success = False
 
         try:
             self.__sock.send(tx_msg)
-            send_msg_success = True 
+            send_msg_success = True
         except:
             logger.error(
                 "Failed to send message to Arbin server!", exc_info=True)
 
         if send_msg_success:
             try:
-                rx_msg += self.__sock.recv(Constants.MSG.BUFFER_SIZE_BYTES)
+                rx_msg += self.__sock.recv(Constants.TX_MSG.BUFFER_SIZE_BYTES)
                 rx_msg_len = struct.unpack('<L', rx_msg[8:12])[0]
                 while len(rx_msg) < rx_msg_len:
-                        rx_msg += self.__sock.recv(Constants.MSG.BUFFER_SIZE_BYTES)
+                    rx_msg += self.__sock.recv(
+                        Constants.TX_MSG.BUFFER_SIZE_BYTES)
             except:
-                logger.error("Error receiving msg", exc_info=True)
-                
+                logger.error("Error receiving TX_MSG", exc_info=True)
+
         return rx_msg
--- pycti/constants.py
+++ pycti/constants.py
@@ -1,17 +1,46 @@
 class Constants:
     
-    class MSG:
+    class TX_MSG:
         TIMEOUT_S = 3
 
         BUFFER_SIZE_BYTES = 2**10
 
+        # Fixed value,	unsigned long long (8 bytes)
+        HEADER_FORMAT = '<Q'
         HEADER = 0x11DDDDDDDDDDDDDD
 
+        MSG_LENGTH_FORMAT = '<L'
+        MSG_LENGTH = {
+            # Command Code (4 bytes) + Extended command code (4 bytes) + Username (32 bytes) + Password (32 bytes) + Checksum (2 bytes)
+            "LOGIN" : 74
+        }
+
+        # Command codes, unsigned long (4 bytes)
+        COMMAND_CODE_FORMAT = '<L'
         COMMAND_CODES = {
-            "USER_LOGIN_CMD": 0xEEAB0001,
+            "LOGIN": 0xEEAB0001,
             "GET_CHANNELS_INFO": 0xEEAB0003,
             "SET_METAVARIABLE_VALUE": 0xBB150001,
             "SCHEDULE_START": 0xBB320004,
             "SCHEDULE_STOP": 0xBB310001,
             "SCHEDULE_JUMP": 0xBB320005
         }
+
+        # Strings are 32 byte character arrays
+        STRING_FORMAT = '32s'
+        STRING_ENCODING = 'ascii'
+
+        # Fixed value 0x00000000, unsigned long (4 bytes)
+        EXTENDED_COMMAND_FORMAT = '<L'
+        EXTENDED_COMMAND_CODE = 0x00000000
+
+    class RX_MSG:
+        class LOGIN:
+            RESULT_START_BYTE = 20
+            RESULT_END_BYTE = 24
+
+            RESULT_FORMAT = 'I'
+
+            SUCESS_RESULT_CODE = 1
+            FAIL_RESULT_CODE = 2
+            ALREADY_LOGGED_IN_CODE = 3
