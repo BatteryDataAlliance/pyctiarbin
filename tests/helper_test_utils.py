7954f04524905d69c9efa1b05403fc4379257ea3
Chintan Pathak <chintan.pathak@gmail.com>
2023-04-14 12:12:59 -0700

    more slight format changes

--- tests/helper_test_utils.py
+++ tests/helper_test_utils.py
@@ -1,49 +1,5 @@
 import os
 import json
-import struct
-from pycti import MessageABC
-
-class TestClass(MessageABC):
-    msg_length = 44
-    command_code = 0x01
-
-    msg_specific_templet = {
-        'test_value': {
-            'format': '<f',
-            'start_byte': 20,
-            'value': 3.14159
-        },
-        'test_string': {
-            'format': '32s',
-            'start_byte': 24,
-            'text_encoding': 'utf-8',
-            'value': 'Test String'
-        },
-    }
-
-def msg_packer(passed_test_value=None) -> bytearray:
-    '''
-    Packs bytearray messages independent of MessageABC
-    '''
-
-    if passed_test_value:
-        test_value = passed_test_value
-    else:
-        test_value = TestClass.msg_specific_templet['test_value']['value']
-
-    test_string = TestClass.msg_specific_templet['test_string']['value']
-    test_string_encoding = TestClass.msg_specific_templet['test_string']['text_encoding']
-
-    key_msg = bytearray([])
-    key_msg += struct.pack('<Q', 0x11DDDDDDDDDDDDDD)
-    key_msg += struct.pack('<L', TestClass.msg_length)
-    key_msg += struct.pack('<L', TestClass.command_code)
-    key_msg += struct.pack('<L', 0x00000000)
-    key_msg += struct.pack('<f', test_value)
-    key_msg += struct.pack('32s', test_string.encode(test_string_encoding))
-    key_msg += struct.pack('<H', sum(key_msg))
-
-    return key_msg
 
 
 def message_file_loader(msg_dir, msg_file_name: str) -> tuple:
--- tests/test_message_abc_methods.py
+++ tests/test_message_abc_methods.py
@@ -1,13 +1,59 @@
 import pytest
-from helper_test_utils import TestClass, msg_packer
+import struct
+from pycti import MessageABC
+
+
+class TestMessageClass(MessageABC):
+    msg_length = 44
+    command_code = 0x01
+
+    msg_specific_templet = {
+        'test_value': {
+            'format': '<f',
+            'start_byte': 20,
+            'value': 3.14159
+        },
+        'test_string': {
+            'format': '32s',
+            'start_byte': 24,
+            'text_encoding': 'utf-8',
+            'value': 'Test String'
+        },
+    }
+
+    @classmethod
+    def msg_packer(cls, passed_test_value=None) -> bytearray:
+        '''
+        Packs bytearray messages independent of MessageABC
+        '''
+
+        if passed_test_value:
+            test_value = passed_test_value
+        else:
+            test_value = cls.msg_specific_templet['test_value']['value']
+
+        test_string = cls.msg_specific_templet['test_string']['value']
+        test_string_encoding = cls.msg_specific_templet['test_string']['text_encoding']
+
+        key_msg = bytearray([])
+        key_msg += struct.pack('<Q', 0x11DDDDDDDDDDDDDD)
+        key_msg += struct.pack('<L', cls.msg_length)
+        key_msg += struct.pack('<L', cls.command_code)
+        key_msg += struct.pack('<L', 0x00000000)
+        key_msg += struct.pack('<f', test_value)
+        key_msg += struct.pack('32s', test_string.encode(test_string_encoding))
+        key_msg += struct.pack('<H', sum(key_msg))
+
+        return key_msg
+
 
 @pytest.mark.messages
 def test_simple_build_msg():
     '''
     Test packing a message with MessageAbc.pack()
     '''
-    key_msg = msg_packer()
-    abc_built_msg = TestClass.pack()
+    key_msg = TestMessageClass.msg_packer()
+    abc_built_msg = TestMessageClass.pack()
     assert (abc_built_msg == key_msg)
 
 
@@ -18,10 +64,10 @@ def test_modify_build_msg():
     '''
     new_test_value = 2.7182
 
-    key_msg = msg_packer(new_test_value)
+    key_msg = TestMessageClass.msg_packer(new_test_value)
 
     update_dict = {'test_value': new_test_value}
-    abc_built_msg = TestClass.pack(update_dict)
+    abc_built_msg = TestMessageClass.pack(update_dict)
     assert (abc_built_msg == key_msg)
 
 
@@ -30,10 +76,10 @@ def test_modify_build_msg_bad_key():
     '''
     Test packing a message with MessageAbc.pack() and modifying it with a bad key
     '''
-    key_msg = msg_packer()
+    key_msg = TestMessageClass.msg_packer()
 
     update_dict = {'bad_value': 15}
-    new_abc_built_msg = TestClass.pack(update_dict)
+    new_abc_built_msg = TestMessageClass.pack(update_dict)
     assert (new_abc_built_msg == key_msg)
 
 
@@ -46,7 +92,7 @@ def test_modify_build_bad_pack():
     key_msg = bytearray([])
 
     update_dict = {'command_code': 'this should not be a string'}
-    new_abc_built_msg = TestClass.pack(update_dict)
+    new_abc_built_msg = TestMessageClass.pack(update_dict)
     assert (new_abc_built_msg == key_msg)
 
 
@@ -65,8 +111,8 @@ def test_parse_msg():
     }
 
     # Prase the message
-    key_msg = msg_packer()
-    prased_msg_dict = TestClass.parse(key_msg)
+    key_msg = TestMessageClass.msg_packer()
+    prased_msg_dict = TestMessageClass.parse(key_msg)
 
     for key in ans_key_dict.keys():
         assert (ans_key_dict[key] == prased_msg_dict[key])
