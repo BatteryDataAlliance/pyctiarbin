10e7e353f003792fd0328c6f7206fe55c1e001a9
Bing Syuan Wang <bingw@battgenie.life>
2023-08-21 16:00:03 -0700

    Update version to 0.0.5

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="pycti-arbin",
-    version="0.0.4",
+    version="0.0.5",
     author="Zander Nevitt, Bing Syuan Wang",
     author_email="info@battgenie.life",
     description="A class based Python interface for communication and control of Arbin cyclers over CTI.",
@@ -13,8 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BattGenie/pycti.git",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
